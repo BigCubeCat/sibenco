aff7bd9cf6d93bcf001a757b335ff35661ea415f
BigCubeCat <bigcubecat@yandex.ru>
2023-08-05 13:00:32 +0700

    fix(mail): rm call send mail function

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-import sendEmail from '../mailer';
 
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-  sendEmail();
   res.send('Hello World');
 });
 
