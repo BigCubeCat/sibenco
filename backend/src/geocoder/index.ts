96f6d57bec3b92765c5a0f7b5d27fbc650281ac1
BigCubeCat <bigcubecat@yandex.ru>
2023-08-23 18:22:33 +0700

    feat(algorithm): create algorithm (not finished)

--- backend/src/geocoder/index.ts
+++ backend/src/geocoder/index.ts
@@ -1,16 +1,28 @@
 import {I_RouterDocument} from '../models/route.model';
 import {makeOptimalRoute} from '../route_machine_api';
-import {RouteData} from '../route_machine_api/types';
+import {getWord} from './goecoder';
 
-export function generateBoxesRoute(route: I_RouterDocument) {
-  const boxes: string[] = [];
-  let optimalRoute: (RouteData | undefined);
-  const fetchApi = async () => {
-    optimalRoute = await makeOptimalRoute([[]]);
-  };
+
+const convertIntoBoxes = async (route: I_RouterDocument) => {
+  const optimalRoute = await makeOptimalRoute([[]]);
   if (optimalRoute === undefined) {
     return [];
   }
-  fetchApi().catch(console.error);
+  let words = new Set<string>([]);
+  for (let i = 0; i < optimalRoute.steps.length; i++) {
+    const step = optimalRoute.steps[i];
+    const word = await getWord('' + step[0], '' + step[1], '');
+    words.add(word);
+  }
+
+  return Array.from(words.values());
+};
+
+export function generateBoxesRoute(route: I_RouterDocument) {
+  let boxes: string[] = [];
+  const fetchConvert = async () => {
+    boxes = await convertIntoBoxes(route);
+  }
+  fetchConvert().catch(console.error);
   return boxes;
 }
