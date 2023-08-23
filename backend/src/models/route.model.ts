e79f855928f29be72519c1200585d1336afcfdfb
reVInotip <grigoriy.novikov220@gmail.com>
2023-08-23 16:14:04 +0700

    create route then create order

--- backend/src/models/route.model.ts
+++ backend/src/models/route.model.ts
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 
 export interface IRouteDoc {
-  points: Array<string>; // TODO
+  points: Array<any>; // TODO
   summary_distance: string;
   ts_number: string;
   special_marks: string;
   driver_name: string;
+  date: string;
 }
 
 export interface I_RouterDocument extends IRouteDoc, mongoose.Document {}
@@ -16,6 +17,7 @@ const RouteShema: mongoose.Schema<I_RouterDocument> = new mongoose.Schema({
   ts_number: {type: String},
   special_marks: {type: String},
   driver_name: {type: String},
+  date: {type: String},
 });
 
 const RouteModel = mongoose.model<I_RouterDocument>('Route', RouteShema);
