07a808e952404414627b931cb2b8d10755937042
Anton Chumak <chumak.04@mail.ru>
2023-08-24 12:45:49 +0700

    deleted javascript files

--- backend/src/properties/durations.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.months = void 0;
-//длительности месяцев в днях (в невисокосном году)
-exports.months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
