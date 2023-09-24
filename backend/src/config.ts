983f06fe2f026a0e16a5f5fe4701f6b70e27cd58
BigCubeCat <bigcubecat@yandex.ru>
2023-09-24 16:32:22 +0700

    feat(config): change config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -54,17 +54,17 @@ export const config: IConfig = {
 };
 
 export function LoadConfig() {
+  const changeConf = (value: string | undefined, defaultValue: string) => value ? value : defaultValue;
   const port = process.env.PORT ? Number(process.env.PORT) : 5000;
-  const url = process.env.MONGO_URL
-    ? process.env.MONGO_URL
-    : 'mongodb://root:toor@127.0.0.1:27017';
+  const url = changeConf(process.env.MONGO_URL, 'mongodb://root:toor@127.0.0.1:27017');
   config.PORT = port;
   config.MONGO_URL = url;
+  config.geocoder.url = changeConf(process.env.GEOWORD_URL, config.geocoder.url);
   config.mail = {
     port: process.env.MAIL_PORT ? Number(process.env.MAIL_PORT) : 587,
-    host: process.env.MAIL_HOST ? process.env.MAIL_HOST : 'smtp.mail.ru',
-    user: process.env.MAIL_USER ? process.env.MAIL_USER : 'user@mail.ru',
-    password: process.env.MAIL_PASS ? process.env.MAIL_PASS : 'qwerty1234',
+    host: changeConf(process.env.MAIL_HOST, 'smtp.mail.ru'),
+    user: changeConf(process.env.MAIL_USER, 'user@mail.ru'),
+    password: changeConf(process.env.MAIL_PASS, 'qwerty1234')
   };
 }
 
--- backend/src/geocoder/index.ts
+++ backend/src/geocoder/index.ts
@@ -4,7 +4,6 @@ import {getWord} from './goecoder';
 import {getOrder} from '../service/order.service';
 import {convertAddressDto} from '../utils/coords';
 import {TOrderDoc} from '../models/order.model';
-import {type} from "os";
 
 export const convertIntoBoxes = async (route: IRouteDoc) => {
   const waypoints: number[][] = [];
@@ -12,25 +11,17 @@ export const convertIntoBoxes = async (route: IRouteDoc) => {
     const orderId = route.route.orders[i];
     const order: TOrderDoc | null = await getOrder(orderId);
     if (!order) continue;
-    console.log("loading = ", order.route.loadingAddress);
-    console.log("unloading = ", order.route.unloadingAddress);
     waypoints.push(await convertAddressDto(order.route.loadingAddress));
     waypoints.push(await convertAddressDto(order.route.unloadingAddress));
   }
-  console.log("waypoints = ", waypoints);
   const optimalRoute = await makeOptimalRoute(waypoints);
-  console.log("optimalRoute = ", optimalRoute);
   if (optimalRoute === undefined) {
     return [];
   }
-  // TODO тут нет ошибки?)
-  console.log("waypoints = ", optimalRoute.waypoints);
   const words = new Set<string>([]);
   for (let i = 0; i < optimalRoute.steps.length; i++) {
     const step = optimalRoute.steps[i];
-    console.log(step);
     const word = await getWord('' + step[0], '' + step[1]);
-    console.log(word);
     if (typeof word === "string") {
       words.add(word);
     }
