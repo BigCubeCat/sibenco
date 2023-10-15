22a3e21a266b88b50f12537a2c1ead1cd526c1b0
Bent0nPi <chumak.04@mail.ru>
2023-10-16 00:56:41 +0700

    fix find  by id

--- backend/src/service/route.service.ts
+++ backend/src/service/route.service.ts
@@ -124,7 +124,7 @@ export async function findSimilarRoutes(id: string): Promise<I_RouterDocument[]>
     if (similarRoutes === null || similarRoutes === undefined) {
       return [];
     }
-    similarRoutes = filterRoutesWithStatus(similarRoutes);
+    similarRoutes = filterRoutesWithStatus(similarRoutes, id);
     let similarRoutesWithBoxes = await getNearestInBoxesRoutes(similarRoutes, route);
     /*if (similarRoutesWithBoxes.length == 1) {
       return similarRoutes;
--- backend/src/utils/routes_filter/routes_by_status.ts
+++ backend/src/utils/routes_filter/routes_by_status.ts
@@ -1,8 +1,8 @@
 import { I_RouterDocument } from "../../models/route.model";
-export function filterRoutesWithStatus(routes: I_RouterDocument[]) {
+export function filterRoutesWithStatus(routes: I_RouterDocument[], routeId: string) {
   let routesWithGoodStatus = [];
   for (let i = 0; i < routes.length; i++) {
-    if (routes[i].status == "merged" || routes[i].isSingle == true || routes[i].isPrivate == true) {
+    if ((routes[i].status == "merged" || routes[i].isSingle == true || routes[i].isPrivate == true) && routes[i]._id != routeId) {
       continue;
     }
     routesWithGoodStatus.push(routes[i]);
