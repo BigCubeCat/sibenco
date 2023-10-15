7a72f3282eb07e8e6aa4c54057bbefbac26c06bd
Чумак Антон Максимович <83911258+Bent0nPi@users.noreply.github.com>
2023-10-15 09:37:26 +0700

    Update routes_by_status.ts

--- backend/src/utils/routes_filter/routes_by_status.ts
+++ backend/src/utils/routes_filter/routes_by_status.ts
@@ -1,6 +1,6 @@
 import { I_RouterDocument } from "../../models/route.model";
 export function filterRoutesWithStatus (routes: I_RouterDocument[]) {
-  routesWithGoodStatus = [];
+  let routesWithGoodStatus = [];
   for (let i = 0; i < routes.length; i++) {
     if (!(routes[i].status == "merged" || routes[i].isSingle == true || routes[i].isPrivate == true)){
       routesWithGoodStatus.push(routes[i]);
